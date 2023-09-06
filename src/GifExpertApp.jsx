875ae4a0b73d6653d30f51da1814b7f93286ecdb
Diego Corredor <lsddiego@hotmail.com>
2023-09-05 21:48:22 -0500

    Primer commit de ap

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components';
+
+
+export const GifExpertApp = () => {
+  
+    
+    // hooks de react que mantienen el estado
+    const [categories,  setCategories]   = useState([ 'One Punch' ]);
+
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes (newCategory) ) return;
+
+
+        setCategories([ newCategory, ...categories ])
+    }
+
+    return (
+    <>
+
+            <h1>GifExpertApp</h1>
+            
+            <AddCategory 
+
+            // setCategories = { setCategories }
+            onNewCategory = { (value) => onAddCategory(value)}
+            currentCategories = { categories }
+            />  
+           
+                { categories.map((category) => 
+                     <GifGrid 
+                            key = { category } 
+                            category={ category} 
+                     />
+                ) }
+            
+
+    </>
+  )
+}
+
+
+
